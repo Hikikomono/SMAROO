831b1f94363d7923e86a161ad84e0bfdea306b94
faelb <faelb@gmx.at>
2020-05-21 20:35:46 +0200

    getState implemented

--- getState.php
+++ getState.php
@@ -5,11 +5,13 @@ $pdo = new PDO("mysql:host=localhost;dbname=smaroo_db", "smaroo", "smaroo");
 $sensortyp = $_GET['Sensortyp']; // checkt welcher table
 
 
-$get_temp_sql = "SELECT state FROM states WHERE sensor = '" . $timestamp . "'";
+$get_temp_sql = "SELECT state FROM states WHERE sensor = '" . $sensortyp . "'";
 
 
 foreach ($pdo->query($get_temp_sql) as $row) {
-$dataRequest = $row['state'];}
+    $dataRequest = $row['state'];
+}
+
 
 echo json_encode($dataRequest);
 ?>
