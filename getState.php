f191882935879dac1a456ddb44d693084e17f44b
faelb <faelb@gmx.at>
2020-05-21 19:09:42 +0200

    troubleshooting 1

--- getState.php
+++ getState.php
@@ -5,13 +5,16 @@ $pdo = new PDO("mysql:host=localhost;dbname=smaroo_db", "smaroo", "smaroo");
 $sensortyp = $_GET['Sensortyp']; // checkt welcher table
 
 
-$get_temp_sql = "SELECT state FROM states WHERE sensor = '" . $timestamp . "'";
+//$get_temp_sql = "SELECT state FROM states WHERE sensor = '" . $timestamp . "'";
 //$temp = $pdo->query($get_temp_sql)->fetch();
+$statement = $pdo->prepare("SELECT state FROM states WHERE sensor = '" . $timestamp . "'");
+$statement->execute();
+$results = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 
-foreach ($pdo->query($get_temp_sql) as $row) {
-    $dataRequest[] = $row;
-}
+//foreach ($pdo->query($get_temp_sql) as $row) {
+ //   $dataRequest[] = $row;
+//}
 
-echo json_encode($dataRequest);
+echo json_encode($results);
 ?>
