0a767156fb9b3c6e8ceaae39286ee84e44679190
faelb <faelb@gmx.at>
2020-06-06 22:16:05 +0200

    ts

--- functions.js
+++ functions.js
@@ -180,7 +180,6 @@ function getLiveData(){
 
 //get data from the Datepicker put it to database and create a chart with chart.js
 function getDatePickerDataAndCreateChart() {
-        var chart = null;
         var dateStart = new Date($('#calStart').val());
         var dateEnd = new Date($('#calEnd').val());
         var diff = (dateEnd-dateStart)/(1000*60*60*24); //berechnet Tage für die Chart erstellung - dargestellte Labels; die Subtraktion ergibt millisekunden
@@ -220,6 +219,7 @@ $('#timespan').text(fromDate +" to " +toDate);
         dataPoints.push("d"+i);
     }
 
+    $('#myChart').empty();
 
 //TODO endDay muss +1 gerechnet werden, weil das sql between aus irgend einem grund nicht inclusive ist
     //TODO das Chart muss clean werden wenn man switcht!
@@ -228,9 +228,10 @@ $('#timespan').text(fromDate +" to " +toDate);
        var obj = JSON.parse(data);
 
 
+
 //now the part for the actual chart creation, inside the request to get the obj
     var ctx = document.getElementById('myChart').getContext('2d');
-    chart = new Chart(ctx, {
+    var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'line',
 
@@ -249,6 +250,6 @@ $('#timespan').text(fromDate +" to " +toDate);
         options: {}
     });
 
-
    }); //ende request
+
 }
